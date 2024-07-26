5fd4a4786ba0116ed54d451b5b3ac2044fe7c810
Neo Han <neohan2016@outlook.com>
2024-07-26 23:42:35 +0000

    added excel file parse

--- code/web_ui/utils/menu.py
+++ code/web_ui/utils/menu.py
@@ -29,7 +29,7 @@ def _check_password():
 def _authenticated_menu():
     # Show a navigation menu for authenticated users
     st.sidebar.page_link("home.py", label="Amazon Bedrock Translate", icon="⚧")
-    # st.sidebar.page_link("pages/user.py", label="Regular User Page", icon="🚎")
+    st.sidebar.page_link("pages/user.py", label="Regular User Page", icon="🚎")
     if st.session_state.role in ["admin", "super-admin"]:
         st.sidebar.page_link("pages/admin.py", label="Dictionary Term Config", icon="🔑")
         st.sidebar.page_link(
